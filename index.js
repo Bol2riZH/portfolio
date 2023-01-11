7ed4fcebf744de3189b77359b5c35c29e9cd5d54
Matthieu <reungoat.matthieu@gmail.com>
2023-01-12 00:10:33 +0100

    add link and navbar animation

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const navbarList = document.querySelector('.navigation__list');
 document.addEventListener('scroll', () => {
   const positionY = window.scrollY;
   if (positionY > 137) {
-    navbar.classList.add('navigation__position-top');
-    navbarList.classList.add('navigation__column');
+    navbar.classList.add('navigation__position--top');
+    navbarList.classList.add('navigation__position--column');
     logo.classList.remove('hidden');
+    logo.classList.add('header__animation');
+    navbar.classList.add('navigation__animation');
   }
   if (positionY < 137) {
-    navbar.classList.remove('navigation__position-top');
-    navbarList.classList.remove('navigation__column');
+    navbar.classList.remove('navigation__position--top');
+    navbarList.classList.remove('navigation__position--column');
     logo.classList.add('hidden');
+    navbar.classList.remove('navigation__animation');
   }
 });

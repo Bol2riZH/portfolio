ff8ad9f701bea004ff76e950a8a97cedb2f85334
Matthieu <reungoat.matthieu@gmail.com>
2023-01-11 08:57:31 +0100

    change navbar position on scroll

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-"use strict";
+'use strict';
+
+const headingTitle = document.querySelector('.header__heading-title');
+const logo = document.querySelector('.header__logo');
+const navbar = document.querySelector('.navigation');
+const navbarList = document.querySelector('.navigation__list');
+document.addEventListener('scroll', () => {
+  const positionY = window.scrollY;
+  if (positionY > 137) {
+    navbar.classList.add('navigation__position-top');
+    navbarList.classList.add('navigation__column');
+  }
+  if (positionY < 137) {
+    navbar.classList.remove('navigation__position-top');
+    navbarList.classList.remove('navigation__column');
+  }
+});

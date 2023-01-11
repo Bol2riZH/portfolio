1639d62ca354ddb95809bd8beb3ccd99f9d0f492
Matthieu <reungoat.matthieu@gmail.com>
2023-01-11 09:20:16 +0100

    add logo on scroll

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const headingTitle = document.querySelector('.header__heading-title');
 const logo = document.querySelector('.header__logo');
 const navbar = document.querySelector('.navigation');
 const navbarList = document.querySelector('.navigation__list');
+
 document.addEventListener('scroll', () => {
   const positionY = window.scrollY;
   if (positionY > 137) {
     navbar.classList.add('navigation__position-top');
     navbarList.classList.add('navigation__column');
+    logo.classList.remove('hidden');
   }
   if (positionY < 137) {
     navbar.classList.remove('navigation__position-top');
     navbarList.classList.remove('navigation__column');
+    logo.classList.add('hidden');
   }
 });

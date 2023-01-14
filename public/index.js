bbeb0ae03beba850f13d0ecddaeceaf92859391e
Matthieu <reungoat.matthieu@gmail.com>
2023-01-14 22:57:02 +0100

    implement nodemailer to send messages

--- public/index.js
+++ public/index.js
@@ -8,15 +8,35 @@ document.addEventListener('scroll', () => {
   const positionY = window.scrollY;
   if (positionY > 137) {
     navbar.classList.add('navigation__position--top');
+    navbar.classList.add('navigation__animation--appearing');
     navbarList.classList.add('navigation__position--column');
     logo.classList.remove('hidden');
     logo.classList.add('header__animation');
-    navbar.classList.add('navigation__animation');
   }
   if (positionY < 137) {
     navbar.classList.remove('navigation__position--top');
+    navbar.classList.remove('navigation__animation--appearing');
     navbarList.classList.remove('navigation__position--column');
     logo.classList.add('hidden');
-    navbar.classList.remove('navigation__animation');
   }
+});
+
+const sendMail = async (mail) => {
+  try {
+    const response = await fetch('http://localhost:3000/send', {
+      method: 'POST',
+      body: mail,
+    });
+    console.log(response);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let mail = new FormData(form);
+  sendMail(mail);
 });
---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const multiparty = require('multiparty');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors({ origin: '*' }));
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
+
+app.route('/').get(function (req, res) {
+  res.sendFile(process.cwd() + '/public/index.html');
+});
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp-mail.outlook.com',
+  port: 587,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+transporter.verify(function (error, success) {
+  !error
+    ? console.log('Server is ready to take our messages')
+    : console.error(error);
+});
+
+app.post('/send', (req, res) => {
+  const form = new multiparty.Form();
+  const data = {};
+
+  form.parse(req, function (error, fields) {
+    console.log(fields);
+    Object.keys(fields).forEach(function (property) {
+      data[property] = fields[property].toString();
+    });
+
+    const mail = {
+      sender: data.email,
+      to: process.env.EMAIL,
+      subject: 'contact via portfolio',
+      text: `${data.name} <${data.email}> \n${data.message}`,
+    };
+
+    transporter.sendMail(mail, (error, data) => {
+      !error
+        ? res.status(200).send('Email successfully sent to recipient!')
+        : res.status(500).send(error + ': Something went wrong.');
+    });
+  });
+});

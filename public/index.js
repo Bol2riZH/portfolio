f549b1ae02c2a10f445ba673af9483676dcfe01b
Matthieu <reungoat.matthieu@gmail.com>
2023-01-15 10:16:40 +0100

    modify fetch url base

--- public/index.js
+++ public/index.js
@@ -2,7 +2,8 @@
 const loaderTimeOut = 1500;
 const popupTimeOut = 1000;
 const spamTimeOut = 1500;
-const urlBase = 'https://www.matthieu-reungoat.com';
+// const urlBase = 'https://www.matthieu-reungoat.com';
+const urlBase = 'https://portfolio-r3g0g3wzd-bol2rizh.vercel.app/';
 
 const root = document.getElementsByTagName('html')[0];
 const loader = document.querySelector('.loader');

05b9016f63ec162b23da4ee9c9afb72fb84e8552
Matthieu <reungoat.matthieu@gmail.com>
2023-01-15 01:52:55 +0100

    add loader and popup

--- public/index.js
+++ public/index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const root = document.getElementsByTagName('html')[0];
+const loader = document.querySelector('.loader');
+const loaderTimeOut = 1500;
+const popupTimeOut = 1000;
+
+window.addEventListener('load', () => {
+  window.scrollTo(0, 0);
+  root.classList.add('no-scroll');
+  setTimeout(() => {
+    loader.classList.add('loader--hidden');
+    root.classList.remove('no-scroll');
+  }, loaderTimeOut);
+});
+
 const logo = document.querySelector('.header__logo');
 const navbar = document.querySelector('.navigation');
 const navbarList = document.querySelector('.navigation__list');
@@ -21,13 +35,26 @@ document.addEventListener('scroll', () => {
   }
 });
 
+const popup = document.querySelector('.popup');
+
 const sendMail = async (mail) => {
+  loader.classList.remove('loader--hidden');
+  loader.style.backgroundColor = 'rgba(25, 23, 23, 0.8)';
+  root.classList.add('no-scroll');
   try {
     const response = await fetch('http://localhost:3000/send', {
       method: 'POST',
       body: mail,
     });
-    console.log(response);
+    if (response.ok) {
+      popup.classList.add('popup--open');
+      loader.classList.add('loader--hidden');
+      root.classList.remove('no-scroll');
+
+      setTimeout(() => {
+        popup.classList.remove('popup--open');
+      }, popupTimeOut);
+    }
   } catch (e) {
     console.error(e);
   }
